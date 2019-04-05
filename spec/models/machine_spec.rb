7fd0735b679a950646d28a3fc4a1000416517b86
wipegup <wipegup@gmail.com>
2019-04-05 10:38:24 -0600

    Model test for machine average price

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Machine, type: :model do
     it {should have_many :snack_machines}
     it {should have_many(:snacks).through :snack_machines}
   end
+
+  describe 'instance methods' do
+    it '#average_price' do
+      @sam = Owner.create(name: "Sam's Snacks")
+      @mix_drink = @sam.machines.create(location: "Don's Mixed Drinks")
+      @snack_1 = @mix_drink.snacks.create(name:"snack_1", price: 1.52)
+      @snack_2 = @mix_drink.snacks.create(name:"snack_2", price: 2.53)
+      @snack_3 = @mix_drink.snacks.create(name:"snack_3", price: 3.54)
+
+      expect(@mix_drink.average_price).to eq(2.53)
+    end
+  end
 end
